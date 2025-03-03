0ab6674dba60242deaeda53a6357bb0a24b828a9
Xiaoyun Xu <xiaxu@made-f0Dr7s9.clusters.42paris.fr>
2025-03-03 19:00:45 +0100

    all cleared

--- day00/ex04/main.c
+++ day00/ex04/main.c
@@ -1,19 +1,44 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+void	init_io(uint8_t value)
+{
+	DDRB |= 0x17;
+	PORTB = value;
+	DDRD &= ~((1 << PD2) | (1 << PD4));
+	PORTD |= (1 << PD2) | (1 << PD4);
+}
+
+uint8_t debounce(uint8_t pin)
+{
+	if (!(PIND & (1 << pin)))
+	{
+		_delay_ms(50);
+		if (!(PIND & (1 << pin)))
+		{
+			while (!(PIND & (1 << pin))) _delay_ms(10);
+			return 1;
+		}
+	}
+	return 0;
+}
+
 int main(void)
 {
-	DDRB |= (1 << PB0);
-	DDRD &= ~(1 << PD2);
-	PORTD |= (1 << PD2);
+	uint8_t value = 0;
+	init_io(value);
 	while (1)
 	{
-		if (!(PIND & (1 << PD2)))
+		if (debounce(PD2))
 		{
-			_delay_ms(50);
-			if (!(PIND & (1 << PD2)))
-				PORTB ^= (1 << PB0);
-			while (!(PIND & (1 << PD2)));
+			if (value == 0x07) value += 9;
+			else if (value < 0x17) value++;
 		}
+		if (debounce(PD4))
+		{
+			if (value == 0x10) value -= 9;
+			else if (value > 0) value--;
+		}
+		PORTB = (value & 0x07) | (value & 0x10);
 	}
 }
