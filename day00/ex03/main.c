aca07b195e4a760566b109a0f7c66b138ebb34fe
Xiaoyun Xu <xiaxu@made-f0Dr7s9.clusters.42paris.fr>
2025-03-03 16:06:43 +0100

    aprem

--- day00/ex03/main.c
+++ day00/ex03/main.c
@@ -6,28 +6,14 @@ int main(void)
 	DDRB |= (1 << PB0);
 	DDRD &= ~(1 << PD2);
 	PORTD |= (1 << PD2);
-	//PORTD &= ~(1 << PD2);
-	/*
-	while (1)
-	{
-		if (!(PIND & (1 << PD2)))
-			PORTB |= (1 << PB0);
-		else
-			PORTB &= ~(1 << PB0);
-	}
-	*/
 	while (1)
 	{
 		if (!(PIND & (1 << PD2)))
 		{
 			_delay_ms(50);
 			if (!(PIND & (1 << PD2)))
-				PORTB |= (1 << PB0);
+				PORTB ^= (1 << PB0);
 			while (!(PIND & (1 << PD2)));
-
-			_delay_ms(50);
-			PORTB &= ~(1 << PB0);
 		}
 	}
-
 }
