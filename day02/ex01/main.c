53332712417848fa93ec651c4f26448d98d197be
Xiaoyun Xu <xiaxu@made-f0Dr8s7.clusters.42paris.fr>
2025-03-08 19:15:24 +0100

    day02 WIP

--- day02/ex01/main.c
+++ day02/ex01/main.c
@@ -1,15 +1,12 @@
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 
 #define UART_BAUDRATE 115200
 #define UBRR (F_CPU / 16 / (UART_BAUDRATE - 1))
 
-// 8N1 : each character is transmitted using 8 bits, no parity bit, 1 stop bit
 void	uart_init(unsigned int ubrr)
 {
 	// datasheet, p.185
-	// USART Baud Rate Register
 	UBRR0H = (unsigned char)(ubrr >> 8);
 	UBRR0L = (unsigned char)ubrr;
 
@@ -26,12 +23,35 @@ void	uart_tx(char c)
 	UDR0 = c;
 }
 
+void	uart_printstr(const char* str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		uart_tx(str[i]);
+		i++;
+	}
+}
+
+void	timer_init()
+{
+	TCCR1B |= (1 << CS12) | (1 << CS10); // prescaler as 1024
+	TCCR1B |= (1 << WGM12);
+	OCR1A = 31250;
+	TIMSK1 |= (1 << OCIE1A);
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+	uart_printstr("Hello World!\r\n");
+}
+
 int main(void)
 {
 	uart_init(UBRR); // Universal Asynchronous Receiver/Transmitter
+	timer_init();
+	sei(); // enable global interrupts
 	while (1)
 	{
-		uart_tx('Z');
-		_delay_ms(1000);
 	}
 }
--- day02/ex03/main.c
+++ day02/ex03/main.c
@@ -5,17 +5,15 @@
 #define UART_BAUDRATE 115200
 #define UBRR (F_CPU / 16 / (UART_BAUDRATE - 1))
 
-// 8N1 : each character is transmitted using 8 bits, no parity bit, 1 stop bit
 void	uart_init(unsigned int ubrr)
 {
 	// datasheet, p.185
-	// USART Baud Rate Register
 	UBRR0H = (unsigned char)(ubrr >> 8);
 	UBRR0L = (unsigned char)ubrr;
 
 	// datasheet, p.201 - 203
 	// UART Control and Status Register
-	UCSR0B = (1 << TXEN0); // enable transmitter
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << RXCIE0); // enable receiver and transmitter
 	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 bits data
 }
 
@@ -26,12 +24,18 @@ void	uart_tx(char c)
 	UDR0 = c;
 }
 
+char	uart_rx(void)
+{
+	while (!(UCSR0A & (1 << RXC0))); // wait for data to be received
+	return UDR0;
+}
+
 int main(void)
 {
 	uart_init(UBRR); // Universal Asynchronous Receiver/Transmitter
 	while (1)
 	{
-		uart_tx('Z');
-		_delay_ms(1000);
+		char c = uart_rx();
+		uart_tx(c);
 	}
 }
